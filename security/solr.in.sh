b90ce9059d2081fb1cd2a41cbd454d2a47315e77
ihab abadi <ihab@utopios.net>
2023-09-01 17:00:26 +0200

    avec support

--- security/solr.in.sh
+++ security/solr.in.sh
@@ -157,19 +157,19 @@
 
 # Enables HTTPS. It is implictly true if you set SOLR_SSL_KEY_STORE. Use this config
 # to enable https module with custom jetty configuration.
-SOLR_SSL_ENABLED=true
+#SOLR_SSL_ENABLED=true
 # Uncomment to set SSL-related system properties
 # Be sure to update the paths to the correct keystore for your environment
-SOLR_SSL_KEY_STORE=etc/solr-ssl.keystore.p12
-SOLR_SSL_KEY_STORE_PASSWORD=secret
-SOLR_SSL_TRUST_STORE=etc/solr-ssl.keystore.p12
-SOLR_SSL_TRUST_STORE_PASSWORD=secret
+#SOLR_SSL_KEY_STORE=etc/solr-ssl.keystore.p12
+#SOLR_SSL_KEY_STORE_PASSWORD=secret
+#SOLR_SSL_TRUST_STORE=etc/solr-ssl.keystore.p12
+#SOLR_SSL_TRUST_STORE_PASSWORD=secret
 # Require clients to authenticate
-SOLR_SSL_NEED_CLIENT_AUTH=false
+#SOLR_SSL_NEED_CLIENT_AUTH=false
 # Enable clients to authenticate (but not require)
-SOLR_SSL_WANT_CLIENT_AUTH=false
+#SOLR_SSL_WANT_CLIENT_AUTH=false
 # Verify client's hostname during SSL handshake
-SOLR_SSL_CLIENT_HOSTNAME_VERIFICATION=false
+#SOLR_SSL_CLIENT_HOSTNAME_VERIFICATION=false
 # SSL Certificates contain host/ip "peer name" information that is validated by default. Setting
 # this to false can be useful to disable these checks when re-using a certificate on many hosts
 #SOLR_SSL_CHECK_PEER_NAME=true
